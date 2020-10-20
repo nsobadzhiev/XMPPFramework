a3decc5105aa882f3f107915d883cea2a23eca7f
Tobias Ottenweller <tobi@voiceup.chat>
2020-10-20 18:43:20 +0200

    More duplicate methods removed.

--- Categories/NSXMLElement+XMPP.m
+++ Categories/NSXMLElement+XMPP.m
@@ -121,42 +121,6 @@
 }
 
 /**
- * This method returns the first child element for the given name (as an NSXMLElement).
- * If no child elements exist for the given name, nil is returned.
-**/
-- (NSXMLElement *)elementForName:(NSString *)name
-{
-	NSArray *elements = [self elementsForName:name];
-	if ([elements count] > 0)
-	{
-		return elements[0];
-	}
-	else
-	{
-		// There is a bug in the NSXMLElement elementsForName: method.
-		// Consider the following XML fragment:
-		// 
-		// <query xmlns="jabber:iq:private">
-		//   <x xmlns="some:other:namespace"></x>
-		// </query>
-		// 
-		// Calling [query elementsForName:@"x"] results in an empty array!
-		// 
-		// However, it will work properly if you use the following:
-		// [query elementsForLocalName:@"x" URI:@"some:other:namespace"]
-		// 
-		// The trouble with this is that we may not always know the xmlns in advance,
-		// so in this particular case there is no way to access the element without looping through the children.
-		// 
-		// This bug was submitted to apple on June 1st, 2007 and was classified as "serious".
-		// 
-		// --!!-- This bug does NOT exist in DDXML --!!--
-		
-		return nil;
-	}
-}
-
-/**
  * This method returns the first child element for the given name and given xmlns (as an NSXMLElement).
  * If no child elements exist for the given name and given xmlns, nil is returned.
 **/
@@ -257,14 +221,6 @@
 - (NSString *)xmlns
 {
 	return [[self namespaceForPrefix:@""] stringValue];
-}
-
-- (void)setXmlns:(NSString *)ns
-{
-	// If we use setURI: then the xmlns won't be displayed in the XMLString.
-	// Adding the namespace this way works properly.
-	
-	[self addNamespace:[NSXMLNode namespaceWithName:@"" stringValue:ns]];
 }
 
 /**
